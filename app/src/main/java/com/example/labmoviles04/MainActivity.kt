8dba98398480f112f811999cb7ca3e6b3770eadc
MarrVL <mariel.valdez@tecsup.edu.pe>
2025-05-30 07:03:10 -0500

    mi prueba de error

--- app/src/main/java/com/example/labmoviles04/MainActivity.kt
+++ app/src/main/java/com/example/labmoviles04/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.labmoviles04.ui.theme.LabMoviles04Theme
 
 class MainActivity : ComponentActivity() {
@@ -37,11 +42,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Composable
+fun ThirdComponent() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "This is the Third Component",
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color.Magenta
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LabMoviles04Theme {
+    Lab04Theme {
         Greeting("Android")
     }
 }
